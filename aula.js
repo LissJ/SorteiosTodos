4165c9ee761411f6136a167898f0216e0acfed72
LissJ <lissj014@gmail.com>
2023-09-05 09:20:53 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,14 @@
+// Math.random gera um número aleatório
+console.log(Math.random());
+
+// Gerando um número aleatório entre 0 e 20
+console.log(Math.random()*20);
+console.log(Math.round(Math.random()*5));
+
+// Math.round arredonda para o inteiro mais próximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// Math.floor arrendonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
